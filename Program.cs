a0571fb462df9b48da182962cf4c998175d70607
Manfred Graf <manfred.graf91@gmail.com>
2023-05-14 22:09:53 +0200

    feat(project): updated SDK and added Program.cs to run and test plugin locally

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using MoBro.Plugin.LibreHardwareMonitor;
+using MoBro.Plugin.SDK.Testing;
+using Serilog.Events;
+
+var plugin = MoBroPluginBuilder
+  .Create<Plugin>()
+  .WithLogLevel(LogEventLevel.Debug)
+  .WithSettings(new Dictionary<string, string>
+  {
+    ["cpu_enabled"] = "true",
+    ["gpu_enabled"] = "true",
+    ["ram_enabled"] = "true",
+    ["motherboard_enabled"] = "true",
+    ["hdd_enabled"] = "true",
+    ["network_enabled"] = "true",
+    ["controller_enabled"] = "true",
+    ["psu_enabled"] = "true",
+    ["battery_enabled"] = "true",
+  })
+  .Build();
+
+Console.ReadLine();
